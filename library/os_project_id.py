357888f4fa1cdea93b6f37cea6881824311f3a16
Matt Pryor <matt.pryor@stfc.ac.uk>
2019-07-23 11:46:14 +0100

    Use private keys from the filesystem to allow multi-user administration

---
+++ library/os_project_id.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: os_project_id
+short_description: Get the current project id.
+extends_documentation_fragment: openstack
+author: Matt Pryor (STFC)
+description:
+  - Gets the current Openstack project ID.
+requirements:
+  - "python >= 2.7"
+  - "openstacksdk"
+options: {}
+'''
+
+EXAMPLES = '''
+---
+
+- name: Get project ID
+  os_project_id:
+
+- debug: var=openstack_project_id
+'''
+
+
+import time
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.openstack import (openstack_full_argument_spec,
+                                            openstack_module_kwargs,
+                                            openstack_cloud_from_module)
+
+
+def main():
+    argument_spec = openstack_full_argument_spec()
+    module_kwargs = openstack_module_kwargs()
+    module = AnsibleModule(argument_spec, **module_kwargs)
+
+    sdk, cloud = openstack_cloud_from_module(module)
+    module.exit_json(
+        changed = False,
+        ansible_facts = dict(openstack_project_id = cloud.current_project.id)
+    )
+
+
+if __name__ == '__main__':
+    main()
